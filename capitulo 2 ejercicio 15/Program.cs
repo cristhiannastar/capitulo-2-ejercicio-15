16cba55c7ee38804e2bb4fd0359c510af9199109
Hp <Hp@DESKTOP-NVV8KB0>
2022-11-16 21:56:22 -0500

    Agregar archivos de proyecto.

---
+++ capitulo 2 ejercicio 15/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ejercicio15
+{
+
+    public class program
+    {
+
+        public static void Main()
+        {
+            int nume = 821;
+            double cuadrado;
+            cuadrado = Math.Pow(nume, 2);
+
+            Console.WriteLine($"el resultado del cuadro 821 es : {cuadrado}");
+
+        }
+    }
+}
